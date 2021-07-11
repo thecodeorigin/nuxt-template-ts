bf2be4d007a8c42b1ce97286cbec392e9fef1a15
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2021-07-11 13:57:13 +0700

    fix(stylelint): fix rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,13 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
   ],
+  plugins: [
+    'stylelint-scss',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
 };
